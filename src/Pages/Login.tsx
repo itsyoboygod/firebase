a469f85fa608d037d4eb86abf878c111ceea0ae0
itsyoboygod <m.vini.ac@hotmail.com>
2022-09-06 14:04:20 -0300

    auth fixed !

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -5,30 +5,30 @@ import {useNavigate} from "react-router-dom"
 export const Login = () => {
     const navigate = useNavigate()
     const signInWithGoogle = async () => {
-    // const result = await signInWithPopup(auth, provider)
-    //     console.log(result)
-    //     navigate("/")
-    // }
-
-
     const result = await signInWithPopup(auth, provider)
-    .then((result) => {
-        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-        const credential = GithubAuthProvider.credentialFromResult(result);
-        const token = credential?.accessToken;
-
-        // The signed-in user info.
-        const user = result.user;
-        // ...
-    }).catch((error) => {
-        const errorMessage = error.message;
-        console.log(errorMessage)
-  });
-
+        console.log(result)
         navigate("/")
+    }
 
 
-}
+//     const result = await signInWithPopup(auth, provider)
+//     .then((result) => {
+//         // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+//         const credential = GithubAuthProvider.credentialFromResult(result);
+//         const token = credential?.accessToken;
+
+//         // The signed-in user info.
+//         const user = result.user;
+//         // ...
+//     }).catch((error) => {
+//         const errorMessage = error.message;
+//         console.log(errorMessage)
+//   });
+
+//         navigate("/")
+
+
+// }
 
     return (
         <>
